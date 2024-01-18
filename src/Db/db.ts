7262d0a15383866a827ef7f27b01288b3308508a
victor828 <veom104@gmail.com>
2024-01-18 15:45:11 -0800

    📦️ [ADD]  se agrego archivo db

---
+++ src/Db/db.ts
@@ -0,0 +1,21 @@
+import { Pool } from "pg";
+require("dotenv").config();
+
+export const pool = new Pool({
+  user: process.env["PGUSER"],
+  host: "localhost",
+  database: process.env["PGDATABASE"],
+  password: process.env["PGPASSWORD"],
+  port: 5432,
+});
+
+export const query = (text: string, params?: string[]) => {
+  return pool.query(text, params);
+};
+
+pool
+  .connect()
+  .then(() => console.log("Conexión exitosa a la base de datos"))
+  .catch((error: any) =>
+    console.error(`Error de conexión a la base de datos: ${error}`)
+  );
