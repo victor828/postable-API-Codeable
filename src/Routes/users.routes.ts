7546b620d606144b2deff87b00bfa03815c209fc
victor828 <veom104@gmail.com>
2024-01-18 16:31:26 -0800

    📝[UPDATE] se actualizo routes de user get all esta completo

--- src/Routes/users.routes.ts
+++ src/Routes/users.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { controller_Users } from "../Controllers/users.controller";
 
 export const routeUser = express.Router();
 
-routeUser.get("/");
+routeUser.get("/", controller_Users.getAll);
 routeUser.get("/");
 routeUser.get("/");
 routeUser.get("/");
