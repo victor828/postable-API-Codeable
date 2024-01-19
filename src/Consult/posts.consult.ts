9cff22763e7ed4ca0bc2c2b90f743fb6e40713ca
victor828 <veom104@gmail.com>
2024-01-19 12:17:56 -0800

    📝[UPDATE] modificacion en la creacion de post/consults

--- src/Consult/posts.consult.ts
+++ src/Consult/posts.consult.ts
@@ -1,11 +1,12 @@
 /** @format */
 
 import { pool } from "../Db/db";
-import { PostsModel, postModel } from "../Models/user.model";
+import { PostsModel, postModel } from "../Models/posts.model";
+import { fecha } from "../Utils/utils";
 
 class Posts {
   async getPosts() {
-    const consult = `SELECT * FROM posts ORDER BY createAt`;
+    const consult = `SELECT * FROM posts`;
     const result = await pool.query(consult);
     return result.rows;
   }
@@ -24,15 +25,22 @@ class Posts {
 
   async createPost(data: postModel) {
     // TODO: agregar la fecha de creacion, crear una funcion que nos de la fecha del momento
-    const consult = `INSERT INTO posts (userId, content, createAt, updateAt) VALUES ($1, $2)`;
-    const result = await pool.query(consult, [data.userId, data.content]);
+    // const consult = `INSERT INTO posts (userId, content, createAt, updateAt)
+    const consult = `INSERT INTO posts (userId, content)
+       VALUES (1, $1) RETURNING *`;
+    // VALUES ($1, $2, $3, $4)`;
+
+    const result = await pool.query(consult, [
+      //   data.userId,
+      data.content
+    ]);
     return result.rows[0];
   }
 
-    async updatePost(data: PostsModel) {
-      // TODO: actualizar la fecha de modificacion
-    const consult = `UPDATE posts SET content = $1 WHERE id = $2`;
-    const result = await pool.query(consult, [data.content, data.id]);
+  async updatePost(data: PostsModel) {
+    // TODO: actualizar la fecha de modificacion
+    const consult = `UPDATE posts SET content = $1 , updateAt = $2 WHERE id = $3`;
+    const result = await pool.query(consult, [data.content, fecha, data.id]);
     return result.rows[0];
   }
 
