1ae444ee132df96468640cc1cc62614b628ab9d5
victor828 <veom104@gmail.com>
2024-01-18 22:04:20 -0800

    📝[UPDATE] se añade getby user en service

--- src/Services/post.service.ts
+++ src/Services/post.service.ts
@@ -1,39 +1,34 @@
 /** @format */
 
 import { consult_Posts } from "../Consult/posts.consult";
-import { post } from "../Models/user.model";
+import { PostsModel } from "../Models/user.model";
 
 class Posts {
   async getAll() {
     const posts = await consult_Posts.getPosts();
     return posts
       ? { ok: true, message: "Posts found", data: posts }
-      : {
-          ok: false,
-          message: "Posts not found",
-          data: null,
-        };
+      : { ok: false, message: "Posts not found", data: null };
   }
   async getPost(id: string) {
     const post = await consult_Posts.getPost(id);
     return post
       ? { ok: true, message: "Post found", data: post }
-      : {
-          ok: false,
-          message: "Post not found",
-          data: null,
-        };
+      : { ok: false, message: "Post not found", data: null };
   }
 
-  async createPost(post: post) {
+  async getPostByUser(user: string) {
+    const post = await consult_Posts.getPost(user);
+    return post
+      ? { ok: true, message: "Posts found", data: post }
+      : { ok: false, message: "Posts not found", data: null };
+  }
+
+  async createPost(post: PostsModel) {
     const newPost = await consult_Posts.createPost(post);
     return newPost
       ? { ok: true, message: "Post created", data: newPost }
-      : {
-          ok: false,
-          message: "Post not created",
-          data: null,
-        };
+      : { ok: false, message: "Post not created", data: null };
   }
 
   //   async updatePost(id, post) {
