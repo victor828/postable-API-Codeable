4db8fbebe95f73e806f5f83998bacbf0b23937c6
Víctor Orozco <veom104@gmail.com>
2024-01-20 01:16:24 -0800

    📝[ADD] añadi la clase Likes y sus metodos correspondientes

--- src/Consult/likes.consult.ts
+++ src/Consult/likes.consult.ts
@@ -2,11 +2,28 @@ import { pool } from "../Db/db";
 
 class Likes {
   async newLike(id: string) {
-    const consult = `insert into likes (iduser, id_post) values($1,$2) returning *`;
-    const response = await pool.query(consult, [1, id]);
-    return consult;
+    const consult = `insert into likes (postid, userid) values($1,$2) returning *`;
+    const response = await pool.query(consult, [id, 1]);
+    console.log(`---> create like: ${JSON.stringify(response.rows)}`);
+
+    return response.rows;
   }
-  async deleteLike() {
-    const consult = `delete from likes where id_user = $1 and id_post = $2 returning *`;
+
+  async deleteLike(id: string) {
+    //   async deleteLike(id: string, userid: string) {
+    const consult = `delete from likes where postid = $1 returning *`;
+    const response = await pool.query(consult, [id]);
+    console.log(`---> delete like: ${JSON.stringify(response.rows)}`);
+
+    return response.rows;
+  }
+
+  async getLikes(id_post: string) {
+    const consult = `select * from likes where postid = $1`;
+    const response = await pool.query(consult, [id_post]);
+    console.log(`---> get likes: ${JSON.stringify(response.rows)}`);
+
+    return response.rows;
   }
 }
+export const consult_Likes = new Likes();
