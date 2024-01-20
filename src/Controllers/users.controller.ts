453b2fce0d6d971e00ca1d12ab41e052c004e201
Víctor Orozco <veom104@gmail.com>
2024-01-19 17:18:27 -0800

    📝[UPDATE] actualizamos algunos datos

--- src/Controllers/users.controller.ts
+++ src/Controllers/users.controller.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 import { service_Users } from "../Services/users.service";
 
 class Users {
-    async getAll (req: Request, res: Response){
-        const r = await service_Users.getAll();
-        r.ok ?  res.status(200).json({r}) : res.status(400);
-        res.status(400).json(r);
-    }
+  async getAll(req: Request, res: Response) {
+    const r = await service_Users.getAll();
+    r.ok ? res.status(200).json({ r }) : res.status(400);
+    res.status(400).json(r);
+  }
 }
 
 export const controller_Users = new Users();
