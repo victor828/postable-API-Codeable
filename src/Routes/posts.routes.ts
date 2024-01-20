89aa145863247b111b6b8f3980ce666c39927c48
Víctor Orozco <veom104@gmail.com>
2024-01-20 01:53:58 -0800

    📝[UPDATE] espacios

--- src/Routes/posts.routes.ts
+++ src/Routes/posts.routes.ts
@@ -1,9 +1,7 @@
 /** @format */
 
 import express from "express";
-
 import { controller_Post } from "../Controllers/posts.controller";
-
 export const routePosts = express.Router();
 
 routePosts.get("/", controller_Post.getAll);
