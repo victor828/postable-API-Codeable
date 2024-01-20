a8b3b3821ea81ffe2cc2d503ad9511c6f7f98c3f
Víctor Orozco <veom104@gmail.com>
2024-01-19 17:27:38 -0800

    📝[UPDATE] se actualizaron datos

--- src/Services/post.service.ts
+++ src/Services/post.service.ts
@@ -1,8 +1,7 @@
-/** @format */
-
 import { consult_Posts } from "../Consult/posts.consult";
-import { users_Consults } from "../Consult/users.consults";
+import { consults_Users } from "../Consult/users.consults";
 import { PostsModel } from "../Models/posts.model";
+// console.log(`---> service: ${id}`);
 
 class Posts {
   async getAll() {
@@ -11,41 +10,29 @@ class Posts {
       ? { ok: true, message: "Posts found", data: posts }
       : { ok: false, message: "Posts not found", data: null };
   }
-  async getPost(id: string) {
-    const post = await consult_Posts.getPost(id);
-    return post
-      ? { ok: true, message: "Post found", data: post }
-      : { ok: false, message: "Post not found", data: null };
-  }
 
-  async getPostByUser(user: string) {
-    /* 
-    1- verificar que el usuario existe
-    2- si existe tomamos si id
-    3- tomamos todos lo spost que le pertenescan al post
-    */
-    const userExist = users_Consults.getUserByName(user);
-    console.log(`service user ${userExist}`);
-    if (!userExist) return { ok: false, message: "El usuario no existe" };
-
-    // const post = await consult_Posts.getPost(userExist.id);
-    // return post
-    return userExist
-      ? { ok: true, message: "Posts found", data: post }
-      : { ok: false, message: "Posts not found", data: null };
+  async getByUser(user: string) {
+    const { id } = await consults_Users.getUserByName(user); //! <--- no sera necesario una vez tengamos la autentificacion neesitamos crear una variable con el id del user
+    const response = await consult_Posts.getPostId(id);
+    return response
+      ? { ok: true, data: response }
+      : { ok: false, data: response };
   }
 
   async createPost(post: PostsModel) {
+    //! Aqui ya tenemos que tener la autentificaion para sar el id -> dentro de create estamos pasando 1 manualmente
     const newPost = await consult_Posts.createPost(post);
     return newPost
       ? { ok: true, message: "Post created", data: newPost }
       : { ok: false, message: "Post not created", data: null };
   }
 
-  //   async updatePost(id, post) {
-  //     const updatedPost = await consult_Posts.updatePost(id, post);
-  //     return updatedPost;
-  //   }
+  async update(data: PostsModel, id: string) {
+    const updatedPost = await consult_Posts.updatePost(data, id);
+    return updatedPost
+      ? { ok: true, message: "Post created", data: updatedPost }
+      : { ok: false, message: "Post not created", data: updatedPost };
+  }
 
   //   async deletePost(id) {
   //     const deletedPost = await consult_Posts.deletePost(id);
