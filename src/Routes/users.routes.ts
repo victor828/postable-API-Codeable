6722227bca8b54e25b067c38e540a092c0575baf
victor828 <veom104@gmail.com>
2024-01-20 11:09:57 -0800

    📝[UPDATE] de rutas

--- src/Routes/users.routes.ts
+++ src/Routes/users.routes.ts
@@ -1,10 +1,12 @@
+/** @format */
+
 import express from "express";
 import { controller_Users } from "../Controllers/users.controller";
 
 export const routeUser = express.Router();
 
 routeUser.get("/", controller_Users.getAll);
-routeUser.get("/");
-routeUser.get("/");
-routeUser.get("/");
+routeUser.get("/me");
+routeUser.post("/login");
+routeUser.post("/register", controller_Users.register);
 routeUser.get("/");
