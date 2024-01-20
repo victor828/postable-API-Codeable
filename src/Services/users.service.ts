fafc60f21c4a63eab1f47c98bb2ff14ce7bbc7b1
Víctor Orozco <veom104@gmail.com>
2024-01-19 17:28:28 -0800

    📝[UPDATE]  se termina de completar la funcion y se crea get post by user

--- src/Services/users.service.ts
+++ src/Services/users.service.ts
@@ -1,13 +1,23 @@
+import { consult_Posts } from "../Consult/posts.consult";
 import { consults_Users } from "../Consult/users.consults";
 
 class Users {
-    async getAll (){
-        const response = await consults_Users.getUsers();
-       return response
-         ?  {ok: true, data: response} 
-         :  {ok: false, data: response};
-
-    }
+  async getAll() {
+    const response = await consults_Users.allUsers();
+    return response
+      ? { ok: true, data: response }
+      : { ok: false, data: response };
+  }
+  async getpostByUser(user: string) {
+    // consulto el usuario y su info
+    const { id } = await consults_Users.getUserByName(user);
+    //tomo la la informacion del usuario y uso su id
+    // consulto los post del usuario
+    const response = await consult_Posts.getPostId(id);
+    return response
+      ? { ok: true, data: response }
+      : { ok: false, data: response };
+  }
 }
 
 export const service_Users = new Users();
