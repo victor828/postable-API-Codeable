ea5688b299953a45fc1f770af1e092222c6bbfa8
Víctor Orozco <veom104@gmail.com>
2024-01-19 17:26:37 -0800

    📝[UPDATE]  se añade patch para posts en routes

--- src/Routes/posts.routes.ts
+++ src/Routes/posts.routes.ts
@@ -9,4 +9,4 @@ export const routePosts = express.Router();
 routePosts.get("/", controller_Post.getAll);
 routePosts.get("/:username", controller_Post.getByUser);
 routePosts.post("/posts", controller_Post.create);
-// routePosts.put("/:id", controller_Post.update);
+routePosts.patch("/posts/:id", controller_Post.update);
