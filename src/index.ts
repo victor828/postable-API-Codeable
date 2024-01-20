af5411010479eaf7363ae07858627f376241362c
Víctor Orozco <veom104@gmail.com>
2024-01-19 17:13:50 -0800

    🔧[UPDATE] actualizamos midleware de rutas

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import { routeUser } from "./Routes/users.routes";
 import { routePosts } from "./Routes/posts.routes";
+import { likesRouter } from "./Routes/likes.routes";
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env["PORT"];
 
 app.use(express.json());
-app.use("/users", routeUser);
-app.use("", routePosts);
+app.use("/me", routeUser);
+app.use("/", routePosts);
+app.use("posts/:postId/likes", likesRouter);
 
 const server = app.listen(PORT, () => {
   const { port } = server.address() as any;
