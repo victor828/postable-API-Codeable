26ae0e8065d4d22f4afc5946658e49114e609258
Víctor Orozco <veom104@gmail.com>
2024-01-20 01:50:36 -0800

    📝[UPDATE] se añadio post y delete

--- src/Routes/likes.routes.ts
+++ src/Routes/likes.routes.ts
@@ -1,4 +1,7 @@
+import { controller_Likes } from "../Controllers/lkes.controller";
+
 const express = require("express");
 export const likesRouter = express.Router();
 
-likesRouter.get("/");
+likesRouter.post("/posts/:postId/like", controller_Likes.newLike);
+likesRouter.delete("/posts/:postId/like", controller_Likes.deleteLike);
