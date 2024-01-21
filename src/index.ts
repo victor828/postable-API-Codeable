b0cddfe68bff2d6d55b9ff7fcfda895470ff3216
Víctor Orozco <veom104@gmail.com>
2024-01-20 22:29:50 -0800

    📝[UPDATE] modificamos el middleware para poder hace otros procesos, posiblemente lo volvamos a modificar adelante

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env["PORT"];
 
 app.use(express.json());
-app.use("/me", routeUser);
+app.use("", routeUser);
 app.use("/", routePosts);
 app.use("/posts", likesRouter);
 
