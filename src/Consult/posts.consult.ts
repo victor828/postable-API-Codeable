5cbdfee5a4ace50da4f910e6d23adce213ee81a3
Víctor Orozco <veom104@gmail.com>
2024-01-20 22:00:10 -0800

    📝[UPDATE] se modifica el quiery para obtener respuesta como se pidio

--- src/Consult/posts.consult.ts
+++ src/Consult/posts.consult.ts
@@ -4,59 +4,98 @@ import { PostsModel, postModel } from "../Models/posts.model";
 class Posts {
   async getPosts() {
     // const consult = `SELECT * FROM posts ORDER BY createdat`;
-       const consult = `SELECT p.*, Count(l.id) as likescount 
-      FROM posts as p
-      LEFT JOIN likes as l ON l.postid = p.id
-      GROUP BY p.id;`;
-    const result = await pool.query(consult);
-    return result.rows;
-  }
-  // * funciona
-  async getPostUserId(id: string) {
-    // const consult = `SELECT * FROM posts WHERE userid = $1`;
-    const consult = `SELECT p.*, Count(l.id) as likescount 
-      FROM posts as p
-      LEFT JOIN likes as l ON l.postid = p.id
-      WHERE p.userid = $1
-      GROUP BY p.id;`;
-    const result = await pool.query(consult, [id]);
-    // console.log(`---> consultas post ${JSON.stringify(result.rows)}`);
-    return result.rows;
+    const consult = `SELECT p.*, u.username, COUNT(l.id) AS likesCount
+FROM posts AS p
+LEFT JOIN users AS u ON p.userid = u.id
+LEFT JOIN likes AS l ON l.postid = p.id
+GROUP BY p.id, u.username`;
+    try {
+      const result = await pool.query(consult);
+      return result.rows;
+    } catch (error) {
+      console.log(error);
+      return {
+        ok: false,
+        error: "error :" + error,
+      };
+    }
   }
 
-  async getPostId(id: string): Promise<postModel> {
+  async getPostUserId(id: string) {
+    const consult = `SELECT p.*, u.username, COUNT(l.id) AS likesCount
+FROM posts AS p
+LEFT JOIN users AS u ON p.userid = u.id
+LEFT JOIN likes AS l ON l.postid = p.id
+WHERE p.userid = $1
+GROUP BY p.id, u.username`;
+    try {
+      const result = await pool.query(consult, [id]);
+      return result.rows;
+    } catch (error) {
+      console.log(error);
+      return {
+        ok: false,
+        error: "error :" + error,
+      };
+    }
+  }
+
+  async getPostId(id: string) {
     console.log(`---> consultas: ${id}`);
     // const consult = `SELECT * FROM posts WHERE id = $1`;
-    const consult = `SELECT p.*, Count(l.id) as likescount 
-      FROM posts as p
-      LEFT JOIN likes as l ON l.postid = p.id
-      WHERE p.id = $1
-      GROUP BY p.id;`;
-    const result = await pool.query(consult, [id]);
-    console.log(`---> consultas post ${JSON.stringify(result.rows[0])}`);
-    return result.rows[0];
+    const consult = `SELECT p.*, u.username, COUNT(l.id) AS likesCount
+FROM posts AS p
+LEFT JOIN users AS u ON p.userid = u.id
+LEFT JOIN likes AS l ON l.postid = p.id
+GROUP BY p.id, u.username
+WHERE p.id = $1`;
+    try {
+      const result = await pool.query(consult, [id]);
+      return result.rows[0];
+    } catch (error) {
+      console.log(error);
+      return {
+        ok: false,
+        error: "error :" + error,
+      };
+    }
   }
 
   async getPostByUser(user: string) {
-    const consult = `SELECT * FROM posts WHERE username = $1`;
-    const result = await pool.query(consult, [user]);
-    return result.rows[0];
+    const consult = `SELECT p.*, u.username, COUNT(l.id) AS likesCount
+      FROM posts AS p
+      LEFT JOIN users AS u ON p.userid = u.id
+      LEFT JOIN likes AS l ON l.postid = p.id
+      GROUP BY p.id, u.username
+      WHERE u.username = $1`;
+    try {
+      const result = await pool.query(consult, [user]);
+      return result.rows[0];
+    } catch (error) {
+      console.log(error);
+      return {
+        ok: false,
+        error: "error :" + error,
+      };
+    }
   }
 
-  async createPost(data: postModel) {
+  async createPost(data: postModel, userId: string) {
     const consult = `INSERT INTO posts (userId, content)
-    VALUES (1, $1) RETURNING *`;
-    // ! el valor 1 corresponde al usurio logueado
-    // TODO: sera cambiado mas adelante por la optencion del id correto
-    const result = await pool.query(consult, [
-      //   data.userId,
-      data.content,
-    ]);
-    return result.rows[0];
+      VALUES ($1, $2) RETURNING *`;
+    try {
+      const result = await pool.query(consult, [userId, data.content]);
+      return result.rows[0];
+    } catch (error) {
+      console.log(error);
+      return {
+        ok: false,
+        error: "error :" + error,
+      };
+    }
   }
 
   async updatePost(data: PostsModel, id: string) {
-    // const consult = `UPDATE posts SET content = $1 , updatedat = NOW() WHERE id = $2 RETURNING *`;
     const consult = `UPDATE posts SET content = $1 , updatedat = NOW() WHERE id = $2 RETURNING *`;
     try {
       const result = await pool.query(consult, [data.content, id]);
@@ -70,8 +109,13 @@ class Posts {
 
   async deletePost(data: PostsModel) {
     const consult = `DELETE FROM posts WHERE id = $1`;
-    const result = await pool.query(consult, [data.id]);
-    return result.rows[0];
+    try {
+      const result = await pool.query(consult, [data.id]);
+      return result.rows[0];
+    } catch (error) {
+      console.error("Error al ejecutar la consulta:", error);
+      throw error;
+    }
   }
 }
 
