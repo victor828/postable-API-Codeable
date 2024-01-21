47032f55b4cf8de801216833e96a6be5dbb39529
Víctor Orozco <veom104@gmail.com>
2024-01-20 22:03:38 -0800

    🗑️[DROP] limpieza de comentarios y agregamos la optencion de datos de usuario logueado

--- src/Controllers/lkes.controller.ts
+++ src/Controllers/lkes.controller.ts
@@ -4,19 +4,15 @@ import { service_Likes } from "../Services/likes.service";
 class Likes {
   async newLike(req: Request, res: Response) {
     const id = req.params["postId"];
-    console.log("este es el param " + id);
-
-    // const { userId } = req.body;
-    // const post = await service_Likes.newLike(id, userId);
-    const post = await service_Likes.newLike(id);
+    const userId = req.body.userId;
+    const post = await service_Likes.newLike(id, userId);
     post.ok ? res.status(201).json(post) : res.status(400).json(post);
   }
 
   async deleteLike(req: Request, res: Response) {
     const id = req.params["postId"];
-    // const { userId } = req.body;
-    // const post = await service_Likes.deleteLike(id, userId);
-    const post = await service_Likes.deleteLike(id);
+    const userId = req.body.userId;
+    const post = await service_Likes.deleteLike(id, userId);
     post.ok ? res.status(201).json(post) : res.status(400).json(post);
   }
 }
