71e1abc1fee4d3074f864588c41d3eddb51275bc
Víctor Orozco <veom104@gmail.com>
2024-01-21 15:56:41 -0800

    📝[UPDATE] modificamos lac constantes que estaban mal escritas

--- src/Controllers/lkes.controller.ts
+++ src/Controllers/lkes.controller.ts
@@ -4,14 +4,14 @@ import { service_Likes } from "../Services/likes.service";
 class Likes {
   async newLike(req: Request, res: Response) {
     const id = req.params["postId"];
-    const userId = req.body.userId;
+    const userId = req.userId;
     const post = await service_Likes.newLike(id, userId);
     post.ok ? res.status(201).json(post) : res.status(400).json(post);
   }
 
   async deleteLike(req: Request, res: Response) {
     const id = req.params["postId"];
-    const userId = req.body.userId;
+    const userId = req.userId;
     const post = await service_Likes.deleteLike(id, userId);
     post.ok ? res.status(201).json(post) : res.status(400).json(post);
   }
