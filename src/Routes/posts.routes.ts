bf2abb74e3b1116fdc556548ed79f62b60a1a64a
Víctor Orozco <veom104@gmail.com>
2024-01-20 22:25:24 -0800

    📝[UPDATE] agregamos los middleware para la autentificacion

--- src/Routes/posts.routes.ts
+++ src/Routes/posts.routes.ts
@@ -2,9 +2,27 @@
 
 import express from "express";
 import { controller_Post } from "../Controllers/posts.controller";
+import { authenticateHandler } from "../midelware/authentication.mdw";
+import { authorize } from "../midelware/authorization.mdw";
+import { posts, schemaValidation } from "../Schema/schemas";
 export const routePosts = express.Router();
 
 routePosts.get("/", controller_Post.getAll);
+
 routePosts.get("/:username", controller_Post.getByUser);
-routePosts.post("/posts", controller_Post.create);
-routePosts.patch("/posts/:id", controller_Post.update);
+
+routePosts.post(
+  "/posts",
+  authenticateHandler,
+  authorize("admin", "users"),
+  schemaValidation(posts),
+  controller_Post.create
+);
+
+routePosts.patch(
+  "/posts/:id",
+  authenticateHandler,
+  authorize("admin", "users"),
+  schemaValidation(posts),
+  controller_Post.update
+);
