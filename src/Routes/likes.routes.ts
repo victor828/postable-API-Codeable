e35b7be9cd391cefbd476f8ca599d68c5bf14da1
Víctor Orozco <veom104@gmail.com>
2024-01-20 22:25:12 -0800

    📝[UPDATE] agregamos los middleware para la autentificacion

--- src/Routes/likes.routes.ts
+++ src/Routes/likes.routes.ts
@@ -1,7 +1,20 @@
 import { controller_Likes } from "../Controllers/lkes.controller";
+import { authenticateHandler } from "../midelware/authentication.mdw";
+import { authorize } from "../midelware/authorization.mdw";
 
 const express = require("express");
 export const likesRouter = express.Router();
 
-likesRouter.post("/posts/:postId/like", controller_Likes.newLike);
-likesRouter.delete("/posts/:postId/like", controller_Likes.deleteLike);
+likesRouter.post(
+  "/posts/:postId/like",
+  authenticateHandler,
+  authorize("admin", "users"),
+  controller_Likes.newLike
+);
+
+likesRouter.delete(
+  "/posts/:postId/like",
+  authenticateHandler,
+  authorize("admin", "users"),
+  controller_Likes.deleteLike
+);
