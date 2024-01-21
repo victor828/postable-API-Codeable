ed037371562840d13cddc6fbbef4742a9b6764f9
Víctor Orozco <veom104@gmail.com>
2024-01-20 22:26:00 -0800

    📝[UPDATE] agregamos los middleware schemas validators

--- src/Routes/users.routes.ts
+++ src/Routes/users.routes.ts
@@ -2,11 +2,12 @@
 
 import express from "express";
 import { controller_Users } from "../Controllers/users.controller";
+import { schemaValidation, userLogin, users } from "../Schema/schemas";
 
 export const routeUser = express.Router();
 
-routeUser.get("/", controller_Users.getAll);
+// routeUser.get("/", controller_Users.getAll);
 routeUser.get("/me");
-routeUser.post("/login");
-routeUser.post("/register", controller_Users.register);
+routeUser.post("/login", schemaValidation(userLogin), controller_Users.login);
+routeUser.post("/signup", schemaValidation(users), controller_Users.register);
 routeUser.get("/");
