8448fbaf2c8395ae4136ab38a8e6d0cbbf76155c
Víctor Orozco <veom104@gmail.com>
2024-01-21 16:02:41 -0800

    📝[ADD] agregamos los metodos para obtener, modificar y eliminar la infromacion del usuario

--- src/Services/users.service.ts
+++ src/Services/users.service.ts
@@ -1,5 +1,6 @@
 /** @format */
 
+import { consult_Likes } from "../Consult/likes.consult";
 import { consult_Posts } from "../Consult/posts.consult";
 import { consults_Users } from "../Consult/users.consults";
 import { user } from "../Models/users.models";
@@ -11,11 +12,39 @@ class Users {
       ? { ok: true, data: response }
       : { ok: false, data: response };
   }
+
+  async getUser(user_id: string) {
+    const response = await consults_Users.getUser(user_id);
+    // necesitamos eliminar la key para que ya no se quede cargando, manejando un error podriamos solucionarlo tambien
+    return response
+      ? { ok: true, data: response }
+      : { ok: false, message: "usuario no existe" };
+  }
+
+  async deleteUser(id_user: string) {
+    const userExist = await consults_Users.getUser(id_user);
+    if (userExist) return { ok: false, message: "usuario no existe" };
+    //* deberia eliminar todos los post que tienen su id ya que  las tablas estan vinculdas con foreign key
+    await consult_Posts.deletePostOfUser(id_user);
+    // TODO: tambien tenemos que eliminar los likes vinculdas
+    await consult_Likes.deleteLikes(id_user);
+    await consults_Users.deleteUser(id_user);
+    return { ok: true, message: "cuenta eliminada" };
+  }
+
+  async updateUser(user_id: string, dataUser: user) {
+    const userExist = await consults_Users.getUserByName(dataUser.username);
+    if (userExist) return { ok: false, message: "nombre de usuario ya existe" };
+
+    const response = await consults_Users.updateUser(dataUser, user_id);
+    return response
+      ? { ok: true, data: response }
+      : { ok: false, data: response };
+  }
+
   async getpostByUser(user: string) {
-    // consulto el usuario y su info
     const { id } = await consults_Users.getUserByName(user);
-    //tomo la la informacion del usuario y uso su id
-    // consulto los post del usuario
+    if (!id) return { ok: false, data: "No existe el usuario" };
     const response = await consult_Posts.getPostId(id);
     return response
       ? { ok: true, data: response }
