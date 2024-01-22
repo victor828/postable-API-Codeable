ae6492520ae4d1dbc7d795339557c739ab47c75c
Víctor Orozco <veom104@gmail.com>
2024-01-21 16:01:21 -0800

    📝[ADD] agregamos los metodos para obtener, modificar y eliminar la infromacion del usuario

--- src/Routes/users.routes.ts
+++ src/Routes/users.routes.ts
@@ -2,12 +2,42 @@
 
 import express from "express";
 import { controller_Users } from "../Controllers/users.controller";
-import { schemaValidation, userLogin, users } from "../Schema/schemas";
+import {
+  schemaValidation,
+  // updateUsers,
+  userLogin,
+  users,
+} from "../Schema/schemas";
+import { authorize } from "../midelware/authorization.mdw";
+import { authenticateHandler } from "../midelware/authentication.mdw";
 
 export const routeUser = express.Router();
 
 // routeUser.get("/", controller_Users.getAll);
-routeUser.get("/me");
+//* Obtener la infromacion de l usuario logeado
+routeUser.get(
+  "/me",
+  authenticateHandler,
+  authorize("admin", "user"),
+  controller_Users.getUser
+);
+
+//* Actualizar la infromacion de l usuario logeado
+routeUser.patch(
+  "/me",
+  // schemaValidation(updateUsers),
+  authenticateHandler,
+  authorize("admin", "user"),
+  controller_Users.updateUser
+);
+
+//* Eliminar la infromacion de l usuario logeado
+routeUser.delete(
+  "/me",
+  authenticateHandler,
+  authorize("admin", "user"),
+  controller_Users.deleteUser
+);
+
 routeUser.post("/login", schemaValidation(userLogin), controller_Users.login);
 routeUser.post("/signup", schemaValidation(users), controller_Users.register);
-routeUser.get("/");
